93dcda0d2211ea30418a55e560bc358cb2c2ae75
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2026-09-24 11:56:38 +0600

    solved task B

---
+++ src/TaskB.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TaskB {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("The next number for the number "+n+" is "+(n+1)+".");
+        System.out.println("The previous number for the number "+n+" is "+(n-1)+".");
+
+
+    }
+
+
+}
