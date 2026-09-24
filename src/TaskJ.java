e6e9f21da29a9883c97c82338cb68d4503ebceb3
jenishbekor <57383078+jenishbekor@users.noreply.github.com>
2026-09-24 13:31:20 +0600

    solved task J

---
+++ src/TaskJ.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TaskJ {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        System.out.println( n + 2 - (n%2));
+        //System.out.println( (n/2 + 1)*2);
+
+
+    }
+
+}
